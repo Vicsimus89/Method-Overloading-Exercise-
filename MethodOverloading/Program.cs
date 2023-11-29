a9cbd634dc89e2aacbfb07d45317da0f41cd0143
Vicsimus89 <fastpitching06801@msn.com>
2023-11-28 22:51:28 -0700

    Added three method overloads of Addthat could take in inputs and return the values expected.

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace MethodOverloading
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MethodOverloading
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+            return a + b;
+        }
+        public static double Add(double a, double b)
+        {
+            Console.WriteLine(a + b);
+            return a + b; 
+        }
+        public static bool Add(int a, int b, bool isTrue)
+        {
+            int y = a + b;
+            int sum = y;
+            if (sum != -1 && sum != 1)
+            {
+                Console.WriteLine($"{sum} dollars");
+                return true;
+                
+            }
+           else
+            {
+                Console.WriteLine($"{sum} dollar");
+                return false;
+            }
+            
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(2, 6);
+
+            Add(8.60, 9.0);
+
+            Add(1, 1, true);
         }
     }
 }
